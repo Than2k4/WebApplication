30128be7e6aa9e694d9adee90c4fc901820d3617
Than2k4 <thanchau.100100@gmail.com>
2024-09-14 11:47:02 +0700

    second

--- main.js
+++ main.js
@@ -1,29 +1,3 @@
-const next = document.querySelector('.next')
-const prev = document.querySelector('.prev')
-const comment = document.querySelector('#list-comment')
-const commentItem = document.querySelectorAll('#list-comment .item')
-var translateY = 0
-var count = commentItem.length
-
-next.addEventListener('click', function (event) {
-    event.preventDefault();
-    if (count == 1) {
-        return false;
-    }
-    translateY += -250;
-    comment.style.transform = `translateY(${translateY}px)`; // Sửa ở đây
-    count--;
-});
-
-prev.addEventListener('click', function (event){
-    event.preventDefault()
-    if (count == 3) {
-        return false
-    }
-    translateY += 250
-    comment.style.transform = `translateY(${translateY}px)`
-    count++
-})
 function toggleForm() {
     const formPopup = document.getElementById('formPopup');
     const overlay = document.getElementById('overlay');
